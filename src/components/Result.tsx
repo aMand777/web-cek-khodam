a56ca86e6f08ab6ea00d60bb0b8678b271dc643f
aMand7 <amanddseventaurus@gmail.com>
2024-06-22 22:03:04 +0700

    feat: add confetti animation on Result component

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useRef, useEffect } from 'react';
+import Confetti from 'react-confetti-boom';
 
 interface ResultProps {
   name: string;
@@ -17,21 +18,24 @@ function Result({ name, khodam, onClick }: ResultProps) {
   }, []);
 
   return (
-    <div className="card w-96 shadow-xl glass-container-card text-white">
-      <div className="card-body">
-        <h1 className="card-title mx-auto text-3xl font-extrabold">Cek Khodam</h1>
-        <p className="text-base mt-3">
-          Khodam pendamping di dalam diri{' '}
-          <span className="text-lg font-semibold text-accent">{name}</span> ada sosok :
-        </p>
-        <h2 className="text-4xl font-extrabold text-accent text-center mt-3 mb-5">{khodam}</h2>
-        <div className="card-actions justify-end">
-          <button ref={buttonRef} onClick={onClick} className="btn btn-secondary px-5">
-            Check lagi
-          </button>
+    <>
+      <Confetti mode="boom" particleCount={50} colors={['#65C3C8', '#EF9FBC', '#EEAF3A']} />
+      <div className="card w-96 shadow-xl glass-container-card text-white">
+        <div className="card-body">
+          <h1 className="card-title mx-auto text-3xl font-extrabold">Cek Khodam</h1>
+          <p className="text-base mt-3">
+            Khodam pendamping di dalam diri{' '}
+            <span className="text-lg font-semibold text-accent">{name}</span> ada sosok :
+          </p>
+          <h2 className="text-4xl font-extrabold text-accent text-center mt-3 mb-5">{khodam}</h2>
+          <div className="card-actions justify-end">
+            <button ref={buttonRef} onClick={onClick} className="btn btn-secondary px-5">
+              Check lagi
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
