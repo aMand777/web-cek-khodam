206d1d0eaaebde33c021c01e787098ab1d0e071a
aMand7 <amanddseventaurus@gmail.com>
2024-06-23 16:24:23 +0700

    feat: add link share to X.com & Whatsap

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -7,6 +7,9 @@ import { randomNumber } from '@/utils';
 import { openModal } from '@/utils';
 import Empty from '@/components/Empty';
 import useInitialName from '@/hook/useInitialName';
+import Whatsapp from '@/components/Whatsapp';
+import X from '@/components/X';
+import Share from '@/components/Share';
 
 interface CardProps {
   data: [''];
@@ -26,6 +29,7 @@ function Card({ data }: CardProps) {
   };
 
   const handleButtonCheck = () => {
+    const randomData = data[Math.floor(Math.random() * data.length)];
     setEmptyKhodam(emptyKhodam + 1);
     setLoading(true);
     setTimeout(() => {
@@ -35,7 +39,6 @@ function Card({ data }: CardProps) {
         setName('');
         setLoading(false);
       } else {
-        const randomData = data[Math.floor(Math.random() * data.length)];
         setKhodam(randomData);
         setLoading(false);
       }
@@ -43,6 +46,7 @@ function Card({ data }: CardProps) {
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    const randomData = data[Math.floor(Math.random() * data.length)];
     if (event.key === 'Enter' && name.length > 0) {
       setEmptyKhodam(emptyKhodam + 1);
       setLoading(true);
@@ -53,7 +57,6 @@ function Card({ data }: CardProps) {
           setName('');
           setLoading(false);
         } else {
-          const randomData = data[Math.floor(Math.random() * data.length)];
           setKhodam(randomData);
           setLoading(false);
         }
@@ -66,6 +69,10 @@ function Card({ data }: CardProps) {
     setName('');
   };
 
+  const message = `Khodam pendamping di dalam diri ${name} ada sosok "${khodam}" Cek khodam kamu disini 👉 https://web-cek-khodam.vercel.app/`;
+  const urlWhatsapp = `https://wa.me/?text=${encodeURIComponent(message)}`;
+  const urlX = `https://x.com/intent/post?url=${encodeURIComponent(message)}`;
+
   return (
     <div>
       <Empty />
@@ -76,7 +83,7 @@ function Card({ data }: CardProps) {
         <Loading name={name} />
       ) : (
         <>
-          {khodam?.length < 1 ? (
+          {khodam.length < 1 ? (
             <Input
               onChange={handleChange}
               onClick={handleButtonCheck}
@@ -91,6 +98,17 @@ function Card({ data }: CardProps) {
       <div className="glass-container-input w-fit mx-auto p-1 rounded-lg mt-5">
         <p className="text-center text-sm text-secondary">Created by @amand</p>
       </div>
+      {khodam.length > 0 && (
+        <div className="mx-auto mt-10 w-fit">
+          <span className="text-white text-sm flex gap-1 items-center mb-3">
+            Share result <Share />{' '}
+          </span>
+          <div className="w-full flex gap-8  justify-center items-center">
+            <Whatsapp href={urlWhatsapp} />
+            <X href={urlX} />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
